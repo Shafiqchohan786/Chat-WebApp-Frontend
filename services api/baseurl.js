6e816930445ea25151a41bdf8e10e826bf104106
Shafiqchohan786 <muhammadshafiqchohan12@gmail.com>
2025-09-15 10:08:45 +0500

    Update baseurl.js

--- services api/baseurl.js
+++ services api/baseurl.js
@@ -1,3 +1,3 @@
 
 //  export const  Baseurl = 'http://localhost:4000'
-export const  Baseurl = 'http://localhost:4000'
+export const  Baseurl = 'http://app-66222205-f0ab-45ab-aa59-c3f0ec8a0d9a.cleverapps.io'
